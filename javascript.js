1c884b39fd8a702c47ff531723def6988f40292f
JosemaArmen <145039986+JosemaArmen@users.noreply.github.com>
2023-09-21 09:57:16 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function hacerPeticion () {
+	let input = document.getElementById("input");
+	let nombrePelicula = input.textContent;
+
+	let respuesta = await fetch('http://www.omdbapi.com/?apikey=8441a313&s=' + nombrePelicula + '&plot=full');
+	let respuestaJson = await respuesta.json();
+
+	let espacioDirector = document.getElementById("espacioDirector");
+	let espacioAnio = document.getElementById("espacioAnio");
+
+	espacioDirector.innerHTML = respuestaJson['Director'];
+	espacioAnio.innerHTML = respuestaJson['Year'];
+}
+
+let boton = document.getElementById("boton");
+boton.addEventListener('click', hacerPeticion);
