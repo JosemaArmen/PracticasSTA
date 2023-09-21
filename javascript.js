07e458c4b1afdd79ea09ca0249dd2a54286a444c
JosemaArmen <145039986+JosemaArmen@users.noreply.github.com>
2023-09-21 13:11:30 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,33 +1,24 @@
-function hacerPeticion2 () {
-	$.ajax({	
-		headers:{  
-			"key":"8441a313",
-			"Accept":"application/json",
-			"Content-type":"application/x-www-form-urlencoded"
-		},
-		url:'http://www.omdbapi.com/?apikey=8441a313&s=' + nombrePelicula + '&plot=full',
-		success:function(response){
-			var r=JSON.parse(response);
-		}
-	});
-
-	return var;
-}
-
 function hacerPeticion () {
 	let input = document.getElementById("input");
 	let nombrePelicula = input.textContent;
 
-	// let respuesta = await fetch('http://www.omdbapi.com/?apikey=8441a313&s=' + nombrePelicula + '&plot=full');
-	// let respuestaJson = await respuesta.json();
-
-	respuestaJson = hacerPeticion2();
-
 	let espacioDirector = document.getElementById("espacioDirector");
 	let espacioAnio = document.getElementById("espacioAnio");
 
-	espacioDirector.innerHTML = respuestaJson['Director'];
-	espacioAnio.innerHTML = respuestaJson['Year'];
+	fetch('http://www.omdbapi.com/?apikey=8441a313&s=' + nombrePelicula + '&plot=full')
+		.then(response => {
+			return response.json();
+				  })
+		.then(respuesta => {
+			espacioDirector.innerHTML = respuesta['Director'];
+			espacioAnio.innerHTML = respuesta['Year'];
+		});
+
+	// let respuesta = await fetch('http://www.omdbapi.com/?apikey=8441a313&s=' + nombrePelicula + '&plot=full');
+	// let respuestaJson = await respuesta.json();
+
+	// espacioDirector.innerHTML = respuestaJson['Director'];
+	// espacioAnio.innerHTML = respuestaJson['Year'];
 }
 
 let boton = document.getElementById("boton");
