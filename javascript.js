ffa969202cce8340b87805dd62f891ee3c1b4fe3
JosemaArmen <145039986+JosemaArmen@users.noreply.github.com>
2023-09-21 17:16:16 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,15 @@
-function hacerPeticion () {
+async function hacerPeticion () {
 	let input = document.getElementById("input");
-	let nombrePelicula = input.value;
+	let nombrePelicula = input.textContent;
 
-	console.log(nombrePelicula);
+	let respuesta = await fetch('http://www.omdbapi.com/?apikey=8441a313&s=' + nombrePelicula + '&plot=full');
+	let respuestaJson = await respuesta.json();
 
 	let espacioDirector = document.getElementById("espacioDirector");
 	let espacioAnio = document.getElementById("espacioAnio");
 
-	fetch('https://www.omdbapi.com/?apikey=8441a313&t=' + nombrePelicula + '&plot=full')
-		.then(response => {
-			return response.json();
-				  })
-		.then(respuesta => {
-			espacioDirector.innerHTML = respuesta['Director'];
-			espacioAnio.innerHTML = respuesta['Year'];
-		});
+	await espacioDirector.innerHTML = respuestaJson['Director'];
+	await espacioAnio.innerHTML = respuestaJson['Year'];
 }
 
 let boton = document.getElementById("boton");
